ccf6654f082ad38cc9e9ed9f9e49d794d42d44f5
pearlrhema <pearlrhema8@gmail.com>
2026-04-01 16:58:21 +0100

    Login front-end, back end/register front-end

--- castYourDotNets/Models/LoginModel.cs
+++ castYourDotNets/Models/LoginModel.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations;
 public class LoginModel
 {
     [Required(ErrorMessage = "Username is required.")]
-    public string Username { get; set; } = string.Empty;
+    public string Username { get; set; } = "";
 
     [Required(ErrorMessage = "Password is required.")]
-    public string Password { get; set; } = string.Empty;
+    public string Password { get; set; } = "";
 
     // Backend errors (like invalid credentials)
     public string? ServerError { get; set; }
